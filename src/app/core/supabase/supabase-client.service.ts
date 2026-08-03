ec694a9a16683fbf38d3656d6139345c2abedbc8
Juan José Merino Brito <jjmerinobrito@gmail.com>
2026-08-03 21:28:42 +0200

    Added supabase/cli to autogenerate types

--- src/app/core/supabase/supabase-client.service.ts
+++ src/app/core/supabase/supabase-client.service.ts
@@ -1,4 +1,13 @@
 import { Service } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+import { environment } from '../../../environments/environment';
+import { Database } from '../models/database.types';
 
 @Service()
-export class SupabaseClientService {}
+export class SupabaseClientService {
+  readonly client: SupabaseClient<Database> = createClient<Database>(
+    environment.supabaseUrl,
+    environment.supabaseKey,
+  );
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://nqqbaggxsbajakztloyc.supabase.co',
+  supabaseKey: 'sb_publishable_Kbg-cQ22WSvfa3Co-H8gkg_IwmV6O5x',
+};
